69a73e57f047501b8549b5b0e78a2e9fd5789574
Mkemp <mkemp63@gmail.com>
2019-05-20 14:53:31 +0200

    test class junit

---
+++ src/test/java/com/example/project/Testing.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+import org.junit.Test;
+import static org.junit.Assert.fail;
+
+public class Testing {
+
+    @Test
+    public void test_fail() {
+        fail("Let's test the failing unit test");
+    }
+}
